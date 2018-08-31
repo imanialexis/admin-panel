dd04ea29c9b752eea2241c9f863d21481e1c7d7a
imani alexis <alexis.bailey37@gmail.com>
2018-08-31 17:11:21 -0400

    added a dashboard page for when user is signed in. will create sep branch for styling of dashboard and functionality

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,12 @@
 class HomepageController < ApplicationController
   def home
+    if current_user 
+      redirect_to '/homepage/dashboard'
+    end
+  end
+
+
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+ get 'homepage/dashboard' => 'homepage#dashboard'
 
 resources :courses
 resources :cohorts

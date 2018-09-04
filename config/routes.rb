b39fa7c7b899fcd23235e9fa36f64977943682d2
imani alexis <alexis.bailey37@gmail.com>
2018-09-04 18:38:52 -0400

    added dashboard page and functionality to boot you to dashboard page on successfull login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- get 'homepage/dashboard' => 'homepage#dashboard'
+ get '/dashboard' => 'homepage#dashboard'
+
 
 resources :courses
 resources :cohorts
